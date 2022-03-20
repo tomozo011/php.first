332d02f8dde46feac8193131a7c2e7a4837b43fc
TOMOZO011 <tomohiroi591@gmail.com>
2022-03-20 14:23:49 +0900

    a

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,9 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email', 191)->index();
             $table->string('token');
-            $table->timestamp('created_at', 191)->nullable();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
